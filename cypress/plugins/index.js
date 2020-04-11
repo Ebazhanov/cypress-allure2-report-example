6b268d7bbc713aeb935581a524b660cf92d24256
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-04-11 14:23:47 +0200

    - updates

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,7 +19,6 @@ module.exports = (on, config) => {};
 module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
-    on('file:preprocessor', cucumber());
     AllureWriter(on);
     return config;
 };
