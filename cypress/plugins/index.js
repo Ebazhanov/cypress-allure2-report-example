b3f71673b1f357a0f88c5f943e64e995af76f988
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-04-15 11:58:58 +0200

    - update versions

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,12 +10,7 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 const AllureWriter = require('@shelex/cypress-allure-plugin/writer');
-module.exports = (on, config) => {};
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-/**
- * @type {Cypress.PluginConfig}
- */
+
 module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
